b134c69858db8c8402854aa1e68058cbbbd45a45
Benard Ronoh <biikate48@gmail.com>
2024-01-03 16:28:05 +0300

    100-createIteratorObject.js.

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,33 +1,9 @@
 export default function createIteratorObject(report) {
-  const allEmployees = report.allEmployees;
-
-  const iterator = {
-    departments: Object.keys(allEmployees),
-    currentDepartmentIndex: 0,
-    currentEmployeeIndex: 0,
-
-    next() {
-      if (this.currentDepartmentIndex < this.departments.length) {
-        const currentDepartment = this.departments[this.currentDepartmentIndex];
-        const employeesInCurrentDepartment = allEmployees[currentDepartment];
-
-        if (this.currentEmployeeIndex < employeesInCurrentDepartment.length) {
-          const currentEmployee =
-            employeesInCurrentDepartment[this.currentEmployeeIndex];
-          this.currentEmployeeIndex++;
-          return { value: currentEmployee, done: false };
-        } else {
-          // Move to the next department
-          this.currentDepartmentIndex++;
-          this.currentEmployeeIndex = 0;
-          return this.next();
-        }
-      } else {
-        // No more departments or employees
-        return { done: true };
+  return (function* _() {
+    for (const department of Object.values(report.allEmployees)) {
+      for (const employee of department) {
+        yield employee;
       }
-    },
-  };
-
-  return iterator;
+    }
+  })();
 }
