053752a4750cc10dbca41e2a9f171576ffe388de
Benard Ronoh <biikate48@gmail.com>
2024-01-03 16:24:48 +0300

    12-createReportObject.js

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,10 @@
+export default function createReportObject(employeesList) {
+  const reportObject = {
+    allEmployees: { ...employeesList },
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
+    },
+  };
+
+  return reportObject;
+}
