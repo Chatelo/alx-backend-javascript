e79b701a639067ab4398c8cfd1e6dc48ca59471a
Benard Ronoh <biikate48@gmail.com>
2024-01-04 10:52:53 +0300

    0-pascal_triangle.py

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,17 @@
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = []
+    for i in range(n):
+        row = [1] * (i + 1)
+        for j in range(1, i):
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+        triangle.append(row)
+
+    return triangle
+
+# # Example usage:
+# n = 5
+# result = pascal_triangle(n)
+# print(result)
