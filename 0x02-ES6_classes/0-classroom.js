575d8ba15c18186601be8e8bb0b221ac4d5bb80f
Benard Ronoh <biikate48@gmail.com>
2024-01-04 11:51:55 +0300

    1-make_classrooms.js

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,19 @@
+export default class ClassRoom {
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+
+  get maxStudentsSize() {
+    return this._maxStudentsSize;
+  }
+
+  set maxStudentsSize(newSize) {
+    if (typeof newSize === "number" && newSize > 0) {
+      this._maxStudentsSize = newSize;
+    } else {
+      console.error(
+        "Invalid input. Please provide a positive number for maxStudentsSize."
+      );
+    }
+  }
+}
