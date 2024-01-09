5ae679935fc9652170a8ca3878c9fee27eda6e95
Benard Ronoh <biikate48@gmail.com>
2024-01-09 13:53:06 +0300

    1-get_list_student_ids.js,

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,8 +1,6 @@
 export default function getListStudentIds(students) {
   if (students instanceof Array) {
-    return students.map((student) => {
-      student.id;
-    });
+    return students.map((student) => student.id);
   }
   return [];
 }
