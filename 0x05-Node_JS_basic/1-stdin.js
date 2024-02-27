1efb80ab152056e5917e0857d6413633bf08163b
Benard Ronoh <biikate48@gmail.com>
2024-02-27 23:31:09 +0300

    1-stdin.js edited

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,13 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.setEncoding('utf8');
-process.stdin.on('data', (input) => {
-  const name = input.trim();
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
 
-  if (name) {
-    process.stdout.write(`Your name is: ${name}\n`);
-  } else {
-    process.stdout.write('This important software is now closing\n');
-    process.exit();
+  if (chunk) {
+    process.stdout.write(`Your name is: ${chunk}`);
   }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
