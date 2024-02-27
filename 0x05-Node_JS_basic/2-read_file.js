b6e5c4318dfd17daf53107096b363b7e516b126f
Benard Ronoh <biikate48@gmail.com>
2024-02-27 23:47:13 +0300

    Checkers

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,10 +1,5 @@
 const fs = require('fs');
 
-/**
- * Counts the students in a CSV data file.
- * @param {String} dataPath The path to the CSV data file.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
- */
 const countStudents = (dataPath) => {
   if (!fs.existsSync(dataPath)) {
     throw new Error('Cannot load the database');
@@ -28,18 +23,22 @@ const countStudents = (dataPath) => {
     if (!Object.keys(studentGroups).includes(field)) {
       studentGroups[field] = [];
     }
-    const studentEntries = studentPropNames
-      .map((propName, idx) => [propName, studentPropValues[idx]]);
+    const studentEntries = studentPropNames.map((propName, idx) => [
+      propName,
+      studentPropValues[idx],
+    ]);
     studentGroups[field].push(Object.fromEntries(studentEntries));
   }
 
-  const totalStudents = Object
-    .values(studentGroups)
-    .reduce((pre, cur) => (pre || []).length + cur.length);
+  const totalStudents = Object.values(studentGroups).reduce(
+    (pre, cur) => (pre || []).length + cur.length
+  );
   console.log(`Number of students: ${totalStudents}`);
   for (const [field, group] of Object.entries(studentGroups)) {
     const studentNames = group.map((student) => student.firstname).join(', ');
-    console.log(`Number of students in ${field}: ${group.length}. List: ${studentNames}`);
+    console.log(
+      `Number of students in ${field}: ${group.length}. List: ${studentNames}`
+    );
   }
 };
 
